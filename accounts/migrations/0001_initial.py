ae14c2babac76c81d94b4fd5e99abead70d6a177
pamidev <pamidev@duck.com>
2023-08-03 19:34:37 +0200

    Make new migrations in accounts app

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -32,6 +32,7 @@ class Migration(migrations.Migration):
                 ('is_player', models.BooleanField(default=True, verbose_name='player')),
                 ('is_judge', models.BooleanField(default=False, verbose_name='judge')),
                 ('is_organizer', models.BooleanField(default=False, verbose_name='organizer')),
+                ('edited', models.DateTimeField(auto_now=True, null=True)),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
