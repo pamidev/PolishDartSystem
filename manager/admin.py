ea9bc4831f34ec98676e9a7b2005d4f81306e473
pamidev <pamidev@duck.com>
2023-08-05 22:33:48 +0200

    Change imported new models names in admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Friend, MatchType, TrainingMatch, Tournament, Competitor, TournamentMatch
+from .models import Friend, MatchType, Training, Tournament, Competitor, Match
 
 admin.site.register(Friend)
 admin.site.register(MatchType)
-admin.site.register(TrainingMatch)
+admin.site.register(Training)
 admin.site.register(Tournament)
 admin.site.register(Competitor)
-admin.site.register(TournamentMatch)
+admin.site.register(Match)
