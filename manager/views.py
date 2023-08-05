0c824358c408bdb2a9db30cd45e7e52f24c6f130
pamidev <pamidev@duck.com>
2023-08-05 22:29:44 +0200

    Add generic views as ListView for Tournament, Competitor and Match models and create simple html pages

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Tournament, Competitor, Match
+
+
+class TournamentListView(ListView):
+    context_object_name = 'tournament_list'
+    queryset = Tournament.objects.all().order_by('-start_date')
+    template_name = 'manager/tournament_list.html'
+
+
+class CompetitorListView(ListView):
+    context_object_name = 'competitor_list'
+    queryset = (Competitor.objects.filter(is_approved=True).select_related('competitor', 'tournament'))
+    template_name = 'manager/competitor_list.html'
+
+
+class MatchListView(ListView):
+    context_object_name = 'match_list'
+    queryset = Match.objects.all().select_related('player_1', 'player_1', 'tournament').order_by('-start_date')
+    template_name = 'manager/match_list.html'
