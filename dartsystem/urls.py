cbf561e73f64ff2c6052069985380cbd8265e291
pamidev <pamidev@duck.com>
2023-08-07 21:29:33 +0200

    Change urlpatterns to include accounts.urls and manager.urls

--- dartsystem/urls.py
+++ dartsystem/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('', include('manager.urls')),
 ]
