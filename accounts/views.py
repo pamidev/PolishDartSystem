f3803902e095f03a795e40ea7f2fad170b399841
pamidev <pamidev@duck.com>
2023-08-11 22:38:22 +0200

    Add view classes with logout and signin in accounts\views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
-from django.utils.decorators import method_decorator
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 
 
@@ -8,11 +6,13 @@ class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
 
 
-@method_decorator(login_required, name='dispatch')
+class CustomLogoutView(LogoutView):
+    template_name = 'accounts/logout.html'
+
+
+class SignInView(TemplateView):
+    template_name = 'accounts/register.html'
+
+
 class DashboardView(TemplateView):
     template_name = 'accounts/dashboard.html'
-
-
-@method_decorator(login_required, name='dispatch')
-class LogOutView(TemplateView):
-    template_name = 'accounts/logout.html'
