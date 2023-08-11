80625f0ea40996aa8ea08a691256e6e9e6e7dfd5
pamidev <pamidev@duck.com>
2023-08-11 22:39:53 +0200

    Add path to signin and modify path to logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 from django.urls import path
 
-from .views import CustomLoginView, LogOutView, DashboardView
+from .views import CustomLoginView, CustomLogoutView, DashboardView, SignInView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogOutView.as_view(), name='logout'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
