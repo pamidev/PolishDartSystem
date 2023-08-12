8e99935134cc033cd2d5686e8683fdc475389b20
pamidev <pamidev@duck.com>
2023-08-12 18:03:12 +0200

    Modify urls.py in manager app

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CompetitorListView, MatchListView, TournamentListView
+from .views import TournamentsListView, HomePageView, TournamentDetailView, CompetitorsListView
 
 urlpatterns = [
-    path('tournaments/', TournamentListView.as_view()),
-    path('tournament/competitors/', CompetitorListView.as_view()),
-    path('tournament/matches/', MatchListView.as_view()),
+    path('', HomePageView.as_view(), name='home'),
+    path('tournaments/', TournamentsListView.as_view(), name='tournaments_list'),
+    path('tournaments/<int:pk>/detail/', TournamentDetailView.as_view(), name='tournament_detail'),
+    path('tournaments/<int:tournament_id>/competitors/', CompetitorsListView.as_view(), name='competitors_list'),
 ]
