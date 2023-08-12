ca696d4599d9dd3273bacf3525e67a3e2d36e949
pamidev <pamidev@duck.com>
2023-08-12 18:05:22 +0200

    Edit views.py in manager app

--- manager/views.py
+++ manager/views.py
@@ -1,21 +1,80 @@
-from django.views.generic import ListView
-
 from .models import Tournament, Competitor, Match
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, TemplateView, DetailView
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import HttpResponseForbidden
 
 
-class TournamentListView(ListView):
-    context_object_name = 'tournament_list'
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class TournamentsListView(ListView):
+    context_object_name = 'tournaments_list'
     queryset = Tournament.objects.all().order_by('-start_date')
-    template_name = 'manager/tournament_list.html'
+    template_name = 'manager/tournaments_list.html'
 
 
-class CompetitorListView(ListView):
-    context_object_name = 'competitor_list'
-    queryset = (Competitor.objects.filter(is_approved=True).select_related('competitor', 'tournament'))
-    template_name = 'manager/competitor_list.html'
+class TournamentDetailView(DetailView):
+    model = Tournament
+    context_object_name = 'tournament_detail'
+    template_name = 'manager/tournament_detail.html'
 
 
-class MatchListView(ListView):
+class CompetitorsListView(ListView):
+    model = Competitor
+    context_object_name = 'competitors_list'
+    template_name = 'manager/competitors_list.html'
+
+    def get_queryset(self):
+        tournament_id = self.kwargs['tournament_id']
+        return Competitor.objects.filter(tournament_id=tournament_id, is_approved=True)
+
+
+class CompetitorDetailView(DetailView):
+    pass
+
+
+class MatchesListView(ListView):
+    object = Match
+    template_name = 'manager/matches_list.html'
     context_object_name = 'match_list'
-    queryset = Match.objects.all().select_related('player_1', 'player_1', 'tournament').order_by('-start_date')
-    template_name = 'manager/match_list.html'
+
+    def get_queryset(self):
+        tournament_id = self.kwargs['tournament_id']
+        return Match.objects.filter(tournament_id=tournament_id).select_related('player_1', 'player_1', 'tournament')
+
+
+class MatchDetailView(DetailView):
+    pass
+
+
+class TrainingsListView(ListView):
+    pass
+
+
+class TrainingDetailView(DetailView):
+    pass
+
+
+# @method_decorator(login_required, name='dispatch')
+# class SetApprovalStatusView(View):
+#
+#     def post(self, request, *args, **kwargs):
+#         tournament_id = kwargs['tournament_id']
+#         competitor_id = kwargs['competitor_id']
+#
+#         tournament = get_object_or_404(Tournament, id=tournament_id)
+#
+#         if not request.user.is_superuser and tournament.organizer != request.user:
+#             return HttpResponseForbidden("Nie masz uprawnień do zarządzania tym turniejem.")
+#
+#         competitor = get_object_or_404(Competitor, id=competitor_id, tournament_id=tournament_id)
+#
+#         is_approved = request.POST.get('is_approved', False)
+#         competitor.is_approved = bool(is_approved)
+#         competitor.save()
+#
+#         return redirect('competitors_list', tournament_id=tournament_id)
