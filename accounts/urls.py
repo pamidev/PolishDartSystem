1d7d9eff78689a9cfc3160061ef7da2d030b7198
pamidev <pamidev@duck.com>
2023-08-13 20:48:15 +0200

    Add new url patterns in accounts\urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,22 @@
-from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 from django.urls import path
 
-from .views import CustomLoginView, CustomLogoutView, DashboardView, SignInView
+from accounts import views
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('signup/', SignInView.as_view(), name='signup'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup/done/', views.SignUpDoneView.as_view(), name='signup_done'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+
+    path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', views.CustomPasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
