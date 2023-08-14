bce2531e72fd57bca3d069fc4ced6fcdb950e35e
pamidev <pamidev@duck.com>
2023-08-14 17:37:12 +0200

    Change imports

--- manager/urls.py
+++ manager/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import TournamentsListView, HomePageView, TournamentDetailView, CompetitorsListView
+from manager import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('tournaments/', TournamentsListView.as_view(), name='tournaments_list'),
-    path('tournaments/<int:pk>/detail/', TournamentDetailView.as_view(), name='tournament_detail'),
-    path('tournaments/<int:tournament_id>/competitors/', CompetitorsListView.as_view(), name='competitors_list'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('tournaments/', views.TournamentsListView.as_view(), name='tournaments_list'),
+    path('tournaments/<int:pk>/detail/', views.TournamentDetailView.as_view(), name='tournament_detail'),
+    path('tournaments/<int:tournament_id>/competitors/', views.CompetitorsListView.as_view(), name='competitors_list'),
 ]
