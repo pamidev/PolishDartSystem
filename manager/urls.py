f2275db0dc13c10dd65f1c3a1a65ce882ab35805
pamidev <pamidev@duck.com>
2023-08-15 22:19:09 +0200

    Add path 'tournaments/add/'

--- manager/urls.py
+++ manager/urls.py
@@ -5,6 +5,7 @@ from manager import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('tournaments/', views.TournamentsListView.as_view(), name='tournaments_list'),
+    path('tournaments/add/', views.TournamentAddView.as_view(), name='tournament_add'),
     path('tournaments/<int:pk>/detail/', views.TournamentDetailView.as_view(), name='tournament_detail'),
     path('tournaments/<int:tournament_id>/competitors/', views.CompetitorsListView.as_view(), name='competitors_list'),
 ]
