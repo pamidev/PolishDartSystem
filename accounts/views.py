73f3bf284317b80bbe3baed4943763197296c750
pamidev <pamidev@duck.com>
2023-08-20 17:09:37 +0200

    Edit ProfileView and ProfileEditView

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
 
@@ -19,24 +20,28 @@ class SignUpDoneView(TemplateView):
 
 class ProfileView(LoginRequiredMixin, DetailView):
     model = CustomUser
-    template_name = 'accounts/profile.html'
+    template_name = 'accounts/profile_details.html'
     context_object_name = 'profile'
 
 
 class ProfileEditView(LoginRequiredMixin, UpdateView):
     model = CustomUser
     fields = [
+        'email',
         'first_name',
         'last_name',
-        'email',
-        'phone',
         'country',
         'city',
+        'phone',
     ]
     template_name = 'accounts/profile_edit.html'
+    context_object_name = 'profile_edit'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(CustomUser, pk=self.request.user.pk)
 
     def get_success_url(self):
-        return reverse('profile', kwargs={'slug': self.object.slug})
+        return reverse('profile', kwargs={'pk': self.object.pk})
 
 
 class CustomLoginView(views.LoginView):
