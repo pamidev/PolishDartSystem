bd61c2d71adbfb895b72fdd6b576ff92f1f0458b
pamidev <pamidev@duck.com>
2023-08-20 23:16:43 +0200

    Delete dashboard path from accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,14 +8,11 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-
     path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
 
     path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile'),
     path('profile/<int:pk>/edit/', views.ProfileEditView.as_view(), name='profile_edit'),
-
 
     path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
