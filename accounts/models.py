106dad45d3709305b0b4543119acf27b157b2e78
pamidev <pamidev@duck.com>
2023-08-20 23:19:07 +0200

    Set null=False in CustomUser fields

--- accounts/models.py
+++ accounts/models.py
@@ -7,9 +7,9 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(max_length=128, unique=True, verbose_name='email address')
-    country = models.CharField(max_length=128, blank=True, null=True)
-    city = models.CharField(max_length=128, blank=True, null=True)
-    phone = models.CharField(max_length=16, blank=True, null=True)
+    country = models.CharField(max_length=128, blank=True, null=False)
+    city = models.CharField(max_length=128, blank=True, null=False)
+    phone = models.CharField(max_length=16, blank=True, null=False)
     is_player = models.BooleanField(default=True, verbose_name='player')
     is_judge = models.BooleanField(default=False, verbose_name='judge')
     is_organizer = models.BooleanField(default=False, verbose_name='organizer')
