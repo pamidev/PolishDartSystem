ab38d9ed8c7bda4918066f132d406afd0914d5b8
pamidev <pamidev@duck.com>
2023-08-20 23:15:33 +0200

    Add path to dashboard view

--- manager/urls.py
+++ manager/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('tournaments/add/', views.TournamentAddView.as_view(), name='tournament_add'),
     path('tournaments/<int:tournament_id>/', views.tournament_details, name='tournament_details'),
     path('tournaments/<int:tournament_id>/competitors/', views.CompetitorsListView.as_view(), name='competitors_list'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
