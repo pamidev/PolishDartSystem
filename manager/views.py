f68ae2a9cfd574801421a16a0810cc644130c8be
pamidev <pamidev@duck.com>
2023-08-23 22:56:04 +0200

    Change querysets in views classes

--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, Q, Case, When, Value, BooleanField
 from django.shortcuts import redirect, get_object_or_404, render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils import timezone
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView
 
 from accounts.models import CustomUser
@@ -17,8 +19,8 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context['num_players'] = CustomUser.objects.filter(is_player=True).count()
-        context['num_judges'] = CustomUser.objects.filter(is_judge=True).count()
+        context['num_players'] = Competitor.objects.filter(is_player=True).count()
+        context['num_judges'] = Competitor.objects.filter(is_judge=True).count()
         context['num_organizers'] = CustomUser.objects.filter(is_organizer=True).count()
         context['num_tournaments'] = Tournament.objects.all().count()
         context['num_matches'] = Match.objects.all().count()
@@ -69,28 +71,16 @@ def dashboard(request):
     if request.user.is_authenticated:
         is_registered = tournament.competitor_set.filter(competitor=request.user).exists()
 
-    is_approved = Competitor.objects.filter(tournament=tournament, is_approved=True, competitor=request.user)
-
     context = {
         'tournaments_as_player': tournaments_as_player,
         'tournaments_as_judge': tournaments_as_judge,
         'tournaments_as_organizer': tournaments_as_organizer,
         'matches_as_player': matches_as_player,
         'matches_as_judge': matches_as_judge,
-        'is_approved': is_approved,
         'is_registered': is_registered,
     }
 
     return render(request, 'manager/dashboard.html', context)
-
-
-class CompetitorsMixin:
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.annotate(
-            approved_competitors=Count('competitor', filter=Q(competitor__is_approved=True))
-        )
-        return queryset
 
 
 class TournamentAddView(LoginRequiredMixin, CreateView):
@@ -105,64 +95,95 @@ class TournamentAddView(LoginRequiredMixin, CreateView):
 
 
 class TournamentEditView(LoginRequiredMixin, UpdateView):
+    model = Tournament
     form_class = TournamentForm
     template_name = 'manager/tournament_edit.html'
-    success_url = reverse_lazy('tournaments_list')
+    context_object_name = 'tournament_edit'
+
+    def get_success_url(self):
+        return reverse('tournaments', kwargs={'pk': self.object.pk})
 
 
-class TournamentsListView(CompetitorsMixin, ListView):
+class TournamentsListView(ListView):
     model = Tournament
     context_object_name = 'tournaments_list'
     template_name = 'manager/tournaments_list.html'
     ordering = 'start_date'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(
+            players=Count('competitor', filter=Q(competitor__is_player=True))
+        )
+        return queryset
+
+
+def is_tournament_organizer(user):
+    return Tournament.objects.filter(organizer=user).exists()
+
+
+@method_decorator(user_passes_test(is_tournament_organizer), name='dispatch')
+class OrganizerTournamentsView(ListView):
+    model = Tournament
+    context_object_name = 'organizer_tournaments'
+    template_name = 'manager/organizer_tournaments.html'
+    ordering = 'start_date'
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tournament.objects.filter(organizer=user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tournaments = context['organizer_tournaments']
+
+        for tournament in tournaments:
+            competitors = Competitor.objects.filter(tournament=tournament).count()
+            tournament.competitors = competitors
+
+        return context
 
 
 @login_required
 def tournament_details(request, tournament_id):
     tournament = get_object_or_404(Tournament, pk=tournament_id)
     is_registered = False
-    has_required_profile_fields = all(
-        getattr(request.user, field_name, None) for field_name in ['first_name', 'last_name', 'email']
-    )
 
     if request.user.is_authenticated:
         is_registered = tournament.competitor_set.filter(competitor=request.user).exists()
 
     if request.method == 'POST':
-        if has_required_profile_fields:
-            form = CompetitorForm(request.POST)
-            if form.is_valid():
-                competitor = form.save(commit=False)
-                competitor.competitor = request.user
-                competitor.tournament = tournament
-                competitor.save()
+        form = CompetitorForm(request.POST)
+        if form.is_valid():
+            competitor = form.save(commit=False)
+            competitor.competitor = request.user
+            competitor.tournament = tournament
+            competitor.joined = timezone.now()
+            competitor.save()
 
-                messages.success(request, 'You have been successfully added as a competitor.')
+            messages.success(request, 'You have been successfully added as a competitor.')
 
-                return redirect('tournament_details', tournament_id=tournament_id)
-        else:
-            missing_fields = [field_name for field_name in ['first_name', 'last_name', 'email']
-                              if not getattr(request.user, field_name, None)]
-            context = {
-                'tournament': tournament,
-                'missing_fields': missing_fields,
-            }
-            return render(request, 'manager/tournament_details.html', context)
+            return redirect('tournament_details', tournament_id=tournament_id)
     else:
         form = CompetitorForm()
 
-    all_competitors = Competitor.objects.filter(tournament=tournament).count()
-    approved_competitors = Competitor.objects.filter(tournament=tournament, is_approved=True).count()
-    is_approved = Competitor.objects.filter(tournament=tournament, is_approved=True, competitor=request.user)
+    num_all_competitors = Competitor.objects.filter(tournament=tournament).count()
+    num_players = Competitor.objects.filter(tournament=tournament, is_player=True).count()
+    num_judges = Competitor.objects.filter(tournament=tournament, is_judge=True).count()
+    player = Competitor.objects.filter(tournament=tournament, is_player=True)
+    judge = Competitor.objects.filter(tournament=tournament, is_judge=True)
+    organizer = Competitor.objects.filter(tournament=tournament, tournament__organizer=request.user)
 
     context = {
         'tournament': tournament,
         'form': form,
-        'all_competitors': all_competitors,
-        'approved_competitors': approved_competitors,
+        'num_all_competitors': num_all_competitors,
+        'num_players': num_players,
+        'num_judges': num_judges,
         'is_registered': is_registered,
-        'is_approved': is_approved,
-        'has_required_profile_fields': has_required_profile_fields,
+        'player': player,
+        'judge': judge,
+        'organizer': organizer,
     }
 
     return render(request, 'manager/tournament_details.html', context)
@@ -175,7 +196,7 @@ class CompetitorsListView(ListView):
 
     def get_queryset(self):
         tournament_id = self.kwargs['tournament_id']
-        return Competitor.objects.filter(tournament_id=tournament_id, is_approved=True)
+        return Competitor.objects.filter(tournament_id=tournament_id, is_player=True)
 
 
 class CompetitorDetailView(DetailView):
